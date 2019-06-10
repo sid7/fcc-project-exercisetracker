7bea56431d34f7a40bf4fb6839cd08851a1de8cc
Glitch (sid7-fcc-project-exercisetracker) <none>
2019-06-10 23:06:47 +0000

    👀🕴 Checkpoint ./models/exercises.js:3898527/205

--- models/exercises.js
+++ models/exercises.js
@@ -1,5 +1,20 @@
 const mongoose = require("mongoose");
 
 const Exercises = new mongoose.Schema({
-  userId: {}
+  userName: String,
+  userId: {
+    type: String,
+    index: true
+  },
+  description: {
+    type: String
+  },
+  duration: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
